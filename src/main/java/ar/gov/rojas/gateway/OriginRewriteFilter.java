f47f0e6a497f1ff3654e9967245e274528084205
Juan Manuel Vazquez <87994357+juanvazquezrouse@users.noreply.github.com>
2026-03-26 10:18:10 -0300

    - initial commit

--- src/main/java/ar/gov/rojas/gateway/OriginRewriteFilter.java
+++ src/main/java/ar/gov/rojas/gateway/OriginRewriteFilter.java
@@ -1,4 +1,31 @@
 package ar.gov.rojas.gateway;
 
-public class OriginRewriteFilter {
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class OriginRewriteFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(org.springframework.web.server.ServerWebExchange exchange,
+                             org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
+
+        ServerHttpRequest request = exchange.getRequest()
+                .mutate()
+                .headers(headers -> {
+                    headers.remove("Origin");
+                    headers.add("Origin", "http://localhost:4200");
+                })
+                .build();
+
+        return chain.filter(exchange.mutate().request(request).build());
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; // alta prioridad
+    }
 }
